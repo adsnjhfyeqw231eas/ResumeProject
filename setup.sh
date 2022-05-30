24060dd7d7b84b81848f1d46e27eefa3f96f77e5
Tridev <tridevguha17@gmail.com>
2022-05-30 18:52:49 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo yum -y update
+sleep 5
+sudo yum -y git httpd
+sleep 5
+sudo amazon-linux-extras install php8.0 mariadb10.5 -y
+sleep 10
+sudo systemctl restart httpd
+sleep 3
+sudo systemctl enable httpd
+sleep 3
+sudo mkdir /var/www/inc
+sleep 2
+cd /home/ec2-user/
+git clone https://github.com/TridevGuha/RDS-S3-Ec2-lab1.git
+sleep 5
+sudo cp /home/ec2-user/RDS-S3-Ec2-lab1/dbinfo.inc /var/www/inc/
+sleep 3
+sudo cp /home/ec2-user/RDS-S3-Ec2-lab1/SamplePage.php /var/www/html/
+sleep 3
