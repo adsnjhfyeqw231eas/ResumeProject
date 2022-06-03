8862a455ada5cdb46cb5932c4c3a6a4bea0114b2
Tridev <tridevguha17@gmail.com>
2022-06-03 16:56:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@
 sudo yum -y update
 sleep 5
 sudo yum -y install git httpd
-sleep 5
-sudo amazon-linux-extras install php8.0 mariadb10.5 -y
 sleep 10
+sudo amazon-linux-extras install php8.0 mariadb10.5 -y
+sleep 17
 sudo systemctl restart httpd
-sleep 3
+sleep 4
 sudo systemctl enable httpd
-sleep 3
+sleep 4
 sudo mkdir /var/www/inc
 sleep 2
 cd /home/ec2-user/
 git clone https://github.com/TridevGuha/RDS-S3-Ec2-lab1.git
-sleep 5
+sleep 7
 sudo cp /home/ec2-user/RDS-S3-Ec2-lab1/dbinfo.inc /var/www/inc/
-sleep 3
+sleep 5
 sudo cp /home/ec2-user/RDS-S3-Ec2-lab1/SamplePage.php /var/www/html/
 sleep 3
